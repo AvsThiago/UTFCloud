4d5e531bcabc1b92acf599e59849a5344265d560
thiag <thiag@DESKTOP-A7IVTFR>
2016-11-07 20:42:36 -0200

    Add project files.

---
+++ Modelo/Arquivos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Arquivos
+    {
+        public long ArquivosID { get; set; }
+        public long RA { get; set; }
+        public bool StPrivado { get; set; }
+        public string Senha { get; set; }
+        public DateTime DtSerRemovido { get; set; }
+        public byte[] Arquivo { get; set; }
+    }
+}
---
+++ Persistencia/Contexts/EFContext.cs
@@ -0,0 +1,27 @@
+﻿using Persistencia.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("UTFCloud")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<EFContext, Configuration>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
